377ae1168af2377bcf6b48bfa7f723cb08cb7f03
devmuturi <muturinjonge09@gmail.com>
2025-07-31 13:30:04 +0300

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 30%',
+      }
+    },
+  },
+  plugins: [],
+}
+
